4374fbf055373c6cb7d1c8a45d34162a68f93295
Ulisse de Candia <ulisse_de-candia.stu@willowschoolnola.org>
2026-06-10 14:12:59 +0000

    removed grid from platform game

--- fsd-projects/platformer/platformer.js
+++ fsd-projects/platformer/platformer.js
@@ -27,7 +27,7 @@ $(function () {
     //////////////////////////////////
 
     // TODO 1 - Enable the Grid
-    toggleGrid();
+    
 
     // TODO 2 - Create Platforms
 createPlatform(0,700,100,10);
